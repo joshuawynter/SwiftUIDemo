94ad29bee843b735a8a1f4ed12cdef64157227b0
Joshua Wynter <joshpursuit@Joshuas-MacBook-Pro.local>
2019-09-24 15:51:56 -0400

    added xcode project

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by Joshua Wynter on 9/24/19.
+//  Copyright © 2019 Joshua Wynter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
